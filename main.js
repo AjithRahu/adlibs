dacae5943f093af673e293c20a62554bf8324386
Ajith Rahu <ajith.rahu@gmail.com>
2017-02-09 04:07:54 +0000

    adlibs

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
+// AD LIBS
+
+
+// YEEZY SECTION
+
+// THAT WAS YEEZY
+
 function sayThatWasEasy() {
-    var thatWasEasy = new Audio("that_was_easy.mp3");
+    var thatWasEasy = new Audio("adlibs/kanye/that_was_easy.mp3");
     thatWasEasy.play();
 }
 
 $("#easy").on("click",sayThatWasEasy); 
+ 
+// HAAAN
 
-$(document).keypress(delgateKeypress);
-function delgateKeypress(event) {
-   if (event.charCode == 32) {
-   var thatWasEasy = new Audio("that_was_easy.mp3");
-    thatWasEasy.play(); 
+function haan() {
+    var thatWasEasy = new Audio("adlibs/kanye/Kanye West Honk (Huh!) Supercut.mp3");
+    thatWasEasy.play();
 }
+
+$("#haan").on("click",haan); 
+ 
+ 
+// SOSA
+
+function sosa() {
+    var thatWasEasy = new Audio("adlibs/sosa/sosa.mp3");
+    thatWasEasy.play();
 }
+
+$("#sosa").on("click",sosa); 
+ 
